a889c648cec69e09cf776dae195ede9187c730c6
Renaldi Hermawan <renaldihermawan65@gmail.com>
2026-06-26 11:11:17 +0700

    add photo upload — column A in sheet, thumbnail in app

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,19 +1,20 @@
 const sheetsLib = require('../../lib/sheets');
 
 const HANDLERS = {
-  setSpreadsheetId: (spreadsheetId, arg) => sheetsLib.setSpreadsheetId(arg),
-  getSuppliers: (spreadsheetId) => sheetsLib.getSuppliers(spreadsheetId),
-  addSupplier: (spreadsheetId, arg) => sheetsLib.addSupplier(spreadsheetId, arg),
-  updateSupplier: (spreadsheetId, arg) => sheetsLib.updateSupplier(spreadsheetId, arg),
-  deleteSupplier: (spreadsheetId, arg) => sheetsLib.deleteSupplier(spreadsheetId, arg),
-  getOrders: (spreadsheetId, arg) => sheetsLib.getOrders(spreadsheetId, arg),
-  addOrder: (spreadsheetId, arg) => sheetsLib.addOrder(spreadsheetId, arg),
-  updateOrder: (spreadsheetId, arg) => sheetsLib.updateOrder(spreadsheetId, arg),
-  deleteOrder: (spreadsheetId, arg) => sheetsLib.deleteOrder(spreadsheetId, arg),
-  addOrdersBulk: (spreadsheetId, arg) => sheetsLib.addOrdersBulk(spreadsheetId, arg),
-  searchBySku: (spreadsheetId, arg) => sheetsLib.searchBySku(spreadsheetId, arg),
-  getAllSkus: (spreadsheetId) => sheetsLib.getAllSkus(spreadsheetId),
-  getExchangeRate: () => sheetsLib.getExchangeRate()
+  setSpreadsheetId:  (spreadsheetId, arg) => sheetsLib.setSpreadsheetId(arg),
+  getSuppliers:      (spreadsheetId)       => sheetsLib.getSuppliers(spreadsheetId),
+  addSupplier:       (spreadsheetId, arg)  => sheetsLib.addSupplier(spreadsheetId, arg),
+  updateSupplier:    (spreadsheetId, arg)  => sheetsLib.updateSupplier(spreadsheetId, arg),
+  deleteSupplier:    (spreadsheetId, arg)  => sheetsLib.deleteSupplier(spreadsheetId, arg),
+  getOrders:         (spreadsheetId, arg)  => sheetsLib.getOrders(spreadsheetId, arg),
+  addOrder:          (spreadsheetId, arg)  => sheetsLib.addOrder(spreadsheetId, arg),
+  updateOrder:       (spreadsheetId, arg)  => sheetsLib.updateOrder(spreadsheetId, arg),
+  deleteOrder:       (spreadsheetId, arg)  => sheetsLib.deleteOrder(spreadsheetId, arg),
+  addOrdersBulk:     (spreadsheetId, arg)  => sheetsLib.addOrdersBulk(spreadsheetId, arg),
+  uploadPhoto:       (spreadsheetId, arg)  => sheetsLib.uploadPhoto(spreadsheetId, arg),
+  searchBySku:       (spreadsheetId, arg)  => sheetsLib.searchBySku(spreadsheetId, arg),
+  getAllSkus:         (spreadsheetId)       => sheetsLib.getAllSkus(spreadsheetId),
+  getExchangeRate:   ()                    => sheetsLib.getExchangeRate()
 };
 
 const NO_SPREADSHEET_NEEDED = new Set(['setSpreadsheetId', 'getExchangeRate']);
